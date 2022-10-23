621f245303cedaa5368ddaf70a05895edfb144f9
Abel Salgado Romero <romeroab@vmware.com>
2022-10-23 21:17:44 +0200

    Inital commit: starter project

---
+++ src/main/java/com/example/boot3ready/Boot3ReadyApplication.java
@@ -0,0 +1,13 @@
+package com.example.boot3ready;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot3ReadyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot3ReadyApplication.class, args);
+	}
+
+}
